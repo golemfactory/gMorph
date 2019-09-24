4469c76b0a533500e8e6c40c6bd4f5ab8c5cc332
Marcin Benke <marcin.benke@gmail.com>
2019-09-24 15:27:24 +0200

    bench_mul

---
+++ benches/mul.rs
@@ -0,0 +1,15 @@
+#![feature(test)]
+
+extern crate test;
+use test::Bencher;
+use gmorph::*;
+
+#[bench]
+fn bench_mul(b: &mut Bencher) {
+    let key_pair = KeyPair::new();
+    let eone = Enc::encrypt(&key_pair, 1);
+
+    b.iter(|| {
+        test::black_box((1..1000).fold(eone, |old, new| old * eone));
+    })
+}
