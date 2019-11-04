1025297f0f0075eeb60e993519389858db183ed9
Marcin Benke <marcin.benke@golem.network>
2019-11-04 11:48:25 +0100

    faster normalize after multiplication (feature-gated) (#11)
    * bench_mul
    
    * Faster normalize after multiplication (experimental)
    
    * Faster experimental normalize_product
    
    * cargo fmt

--- src/algebra/m231.rs
+++ src/algebra/m231.rs
@@ -86,6 +86,16 @@ fn normalize_u64(mut v: u64) -> u32 {
 }
 
 #[inline]
+// should work for arguments up to (MODULUS-1)*(MODULUS-1)
+// which is the case for products of the form a * b where a,b < MODULUS
+fn normalize_product(mut v: u64) -> u32 {
+    v = (v >> 31) + (v & MODULUSU64);
+    v = (v >> 31) + (v & MODULUSU64); // sic!
+
+    v as u32
+}
+
+#[inline]
 fn normalize(x: u32) -> u32 {
     modulo(x)
 }
@@ -113,8 +123,13 @@ impl AddAssign for Mod231 {
 impl Mul for Mod231 {
     type Output = Self;
 
+    #[cfg(not(feature = "experimental-normalize"))]
     fn mul(self, other: Self) -> Self::Output {
         Self(normalize_u64(self.0 as u64 * other.0 as u64))
+    }
+    #[cfg(feature = "experimental-normalize")]
+    fn mul(self, other: Self) -> Self::Output {
+        Self(normalize_product(self.0 as u64 * other.0 as u64))
     }
 }
 
@@ -226,7 +241,22 @@ mod tests {
 
     #[quickcheck]
     fn prop_normalize(x: u32) -> bool {
-        normalize(x) == ((x % MODULUS) + MODULUS) % MODULUS
+        normalize(x) == (x % MODULUS)
+    }
+
+    #[quickcheck]
+    fn prop_normalize_u64(x: u64) -> bool {
+        normalize_u64(x) == (x % MODULUSU64) as u32
+    }
+
+    #[quickcheck]
+    fn prop_normalize_product(a: u32, b: u32) -> TestResult {
+        if a >= MODULUS || b >= MODULUS {
+            return TestResult::discard();
+        }
+        let product = a as u64 * b as u64;
+        let prop = normalize_product(product) == (product % MODULUSU64) as u32;
+        TestResult::from_bool(prop)
     }
 
     #[quickcheck]
